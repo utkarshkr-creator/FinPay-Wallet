c76e4bb694c61fbddd57f39988f04ca81a222d46
utkarsh kumar <2016krutkarsh@gmail.com>
2024-04-06 22:05:17 +0530

    Merchant and user Authentication using NextAuth and Appbar after login

---
+++ packages/db/prisma/migrations/20240401091516_paytm_db/migration.sql
@@ -0,0 +1,31 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[phoneNumber]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `phoneNumber` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "AuthType" AS ENUM ('Google', 'Github');
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "password" TEXT NOT NULL,
+ADD COLUMN     "phoneNumber" TEXT NOT NULL,
+ALTER COLUMN "email" DROP NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Merchant" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT,
+    "name" TEXT,
+    "auth_type" "AuthType" NOT NULL,
+
+    CONSTRAINT "Merchant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Merchant_email_key" ON "Merchant"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phoneNumber_key" ON "User"("phoneNumber");
---
+++ packages/utils/src/commonEnums.ts
@@ -0,0 +1,4 @@
+export enum AuthType {
+  Google,
+  Github
+}
